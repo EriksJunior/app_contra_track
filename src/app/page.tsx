29c54024b12fd049b9fe74308aa0a5d2793138bc
eriks junior <eriksjunior13@gmail.com>
2025-01-07 21:22:52 -0300

    feat: iniciado contra track

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+
+export async function generateMetadata() {
+  return {
+    title: "Contra Track",
+    description: `Veja aqui todos os ducomentos fiscais emitidos para você!`,
+    generator: "Contra Track",
+    applicationName: "Contra Track",
+    keywords: [
+      'Notas Fiscais',
+      'Cupons fiscais',
+      'Notas fiscais de serviço',
+      'Notas de serviço',
+      'Manifesto de transporte',
+      'Manifesto Eletronico',
+      'NF-e',
+      'NFC-e',
+      'NFS-e',
+      'MDF-e'
+    ],
+    authors: [
+      { name: "Contra Track" },
+      { name: "Contra Track", url: "" },
+    ],
+    creator: "Contra Track",
+    publisher: "Contra Track",
+    category: "Documentos Fiscais",
+    formatDetection: {
+      email: false,
+      address: false,
+      telephone: false,
+    },
+  }
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className={styles.main}>
+      <p>HOME</p>
+    </main>
   );
 }
