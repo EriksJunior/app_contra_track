99cfe0cae876755f4594ec49663010acd10a6032
EriksJunior <Eriksjunior13@gmail.com>
2025-01-15 00:39:38 -0300

    feat: adicionado card padrão na home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,52 @@
-import styles from './page.module.css'
+"use client"
 
-export async function generateMetadata () {
-  return {
-    title: 'Contra Track',
-    description: 'Veja aqui todos os ducomentos fiscais emitidos para você!',
-    generator: 'Contra Track',
-    applicationName: 'Contra Track',
-    keywords: [
-      'Notas Fiscais',
-      'Cupons fiscais',
-      'Notas fiscais de serviço',
-      'Notas de serviço',
-      'Manifesto de transporte',
-      'Manifesto Eletronico',
-      'NF-e',
-      'NFC-e',
-      'NFS-e',
-      'MDF-e'
-    ],
-    authors: [
-      { name: 'Contra Track' },
-      { name: 'Contra Track', url: '' }
-    ],
-    creator: 'Contra Track',
-    publisher: 'Contra Track',
-    category: 'Documentos Fiscais',
-    formatDetection: {
-      email: false,
-      address: false,
-      telephone: false
-    }
-  }
-}
+import { useContext } from "react"
+import { ThemeContext } from "@/context/theme"
 
-export default function Home () {
+import { Card } from "./components/UI/Card"
+import { Title } from "./components/UI/Title"
+
+// export async function generateMetadata() {
+//   return {
+//     title: 'Contra Track',
+//     description: 'Veja aqui todos os ducomentos fiscais emitidos para você!',
+//     generator: 'Contra Track',
+//     applicationName: 'Contra Track',
+//     keywords: [
+//       'Notas Fiscais',
+//       'Cupons fiscais',
+//       'Notas fiscais de serviço',
+//       'Notas de serviço',
+//       'Manifesto de transporte',
+//       'Manifesto Eletronico',
+//       'NF-e',
+//       'NFC-e',
+//       'NFS-e',
+//       'MDF-e'
+//     ],
+//     authors: [
+//       { name: 'Contra Track' },
+//       { name: 'Contra Track', url: '' }
+//     ],
+//     creator: 'Contra Track',
+//     publisher: 'Contra Track',
+//     category: 'Documentos Fiscais',
+//     formatDetection: {
+//       email: false,
+//       address: false,
+//       telephone: false
+//     }
+//   }
+// }
+
+export default function Home() {
+  const { theme } = useContext(ThemeContext)
+
   return (
-    <main className={styles.main}>
-      <p>HOME</p>
-    </main>
+    <div style={{ padding: "15px 15px 15px 0", height: '100%' }}>
+      <Card theme={theme} height="100%">
+        <Title fontSize="20px" color="black" text="Home" />
+      </Card>
+    </div>
   )
 }
